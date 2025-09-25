f939c2b4aaad2e1857c84eb8a4696f4fe2b12af8
chrisd94 <chris.diepolder@web.de>
2025-09-25 15:12:22 +0200

    CEFUN-72: introduce a NotificationService for notifying subscriber about call updates via mail

---
+++ backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/out/fundify/EmailService.java
@@ -0,0 +1,55 @@
+package at.ac.tuwien.fundify.adapters.out.fundify;
+
+import at.ac.tuwien.fundify.application.port.out.notification.NotificationService;
+import at.ac.tuwien.fundify.domain.funding.Call;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import lombok.extern.jbosslog.JBossLog;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+@JBossLog
+public class EmailService implements NotificationService {
+
+  public EmailService(Mailer mailer, EmailFactory emailFactory) {
+    this.mailer = mailer;
+    this.emailFactory = emailFactory;
+  }
+
+  private final Mailer mailer;
+  private final EmailFactory emailFactory;
+
+  private final ConcurrentLinkedQueue<Mail> pendingEmails = new ConcurrentLinkedQueue<>();
+  @ConfigProperty(name = "fundify.notification.email.batch-size")
+  Integer batchSize;
+
+  @Override
+  public void addNotificationToQueue(Call call) {
+    log.info("Adding notification for call to queue: " + call);
+
+    List<Mail> mails = Optional.ofNullable(call.getSubscriptions())
+        .stream()
+        .flatMap(List::stream)
+        .map(sub -> emailFactory.createNotificationEmail(sub, call))
+        .toList();
+
+    pendingEmails.addAll(mails);
+  }
+
+  @Scheduled(every = "{every.interval.email-poll}")
+  public void processPendingEmails() {
+
+      if (pendingEmails.isEmpty()) {
+        return;
+      }
+
+      for (int i = 0; i < batchSize; i++) {
+        mailer.send(pendingEmails.poll());
+      }
+  }
+}
