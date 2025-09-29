29d3cf1db2f114dac5c43c341872f7e42dd33ca4
Martin Schwarz <martin.schwarz@andamp.io>
2025-09-29 14:11:41 +0200

    [CEFUN-77]: delete program ngrx store

--- frontend/src/app/programs/store/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './programs.reducers';
-export * from './programs.effects';
-export * from './programs.actions';
-export * from './programs.selectors';
-export * from './programs.state';
