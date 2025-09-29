b1cbc1f08d70aec3ca84064fd7ff8230a6ea0893
Martin Schwarz <martin.schwarz@andamp.io>
2025-09-29 14:11:41 +0200

    [CEFUN-77]: delete program ngrx store

--- frontend/src/app/funders/funders.module.ts
+++ frontend/src/app/funders/funders.module.ts
@@ -5,16 +5,11 @@ import { SharedModule } from '../shared/shared.module';
 import { FunderDisplayPipe } from './pipes/funder-display.pipe';
 import { FunderDetailEditComponent } from './components/funder-detail-edit/funder-detail-edit.component';
 import { FunderDetailComponent } from './components/funder-detail/funder-detail.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { FundersEffects, fundersReducer } from './store';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    StoreModule.forFeature('funders', fundersReducer),
-    EffectsModule.forFeature([FundersEffects]),
     FundersComponent,
     FunderDisplayPipe,
     FunderDetailEditComponent,
