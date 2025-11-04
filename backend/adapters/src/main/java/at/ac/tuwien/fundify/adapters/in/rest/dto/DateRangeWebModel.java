e8625eb90de594b27476127c247742b193f3ba0b
Martin Schwarz <martin.schwarz@andamp.io>
2025-11-04 09:12:33 +0100

    [CEFUN-113]: mark start and end date for DateRangeWebModel as nullable

--- backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/DateRangeWebModel.java
+++ backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/DateRangeWebModel.java
@@ -1,13 +1,10 @@
 package at.ac.tuwien.fundify.adapters.in.rest.dto;
 
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 
 public record DateRangeWebModel(
-        @NotNull
         LocalDateTime start,
-        @NotNull
         LocalDateTime end
 ) {
 }
