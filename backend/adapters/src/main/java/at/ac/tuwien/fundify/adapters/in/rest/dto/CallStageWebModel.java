c0d3b33e5fb23585d5d067aae1050cc3c6ceb3f7
Martin Schwarz <martin.schwarz@andamp.io>
2025-11-18 10:42:16 +0100

    [CEFUN-119]: mark description for callstage as optional

--- backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/CallStageWebModel.java
+++ backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/CallStageWebModel.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.fundify.adapters.in.rest.dto;
 
-import java.util.List;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 
@@ -9,7 +9,7 @@ public record CallStageWebModel(
         int number,
         @Schema(required = true) @NotNull
         DateRangeWebModel duration,
-        @Schema(required = true) @NotNull
+        @Schema(required = false)
         List<TranslatedTextWebModel> description
 ) {
 }
