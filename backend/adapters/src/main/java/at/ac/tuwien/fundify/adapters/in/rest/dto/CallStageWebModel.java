478543a55c261e9cc9369e6c2f74f296a970cfdd
juliakummer <julia.kummer@andamp.io>
2026-04-22 15:37:03 +0200

    [CEFUN-147]: remove unused imports

--- backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/CallStageWebModel.java
+++ backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/CallStageWebModel.java
@@ -1,8 +1,6 @@
 package at.ac.tuwien.fundify.adapters.in.rest.dto;
 
 import java.util.List;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-
 
 public record CallStageWebModel(
         int number,
