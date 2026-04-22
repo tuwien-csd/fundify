b1d30c3e96bd51f060da2b119499d8d8c0d831cf
juliakummer <julia.kummer@andamp.io>
2026-04-22 09:59:17 +0200

    [CEFUN-145]: extend career stage enums with additional fields

--- frontend/src/app/shared/models/enums/career-stage.enum.ts
+++ frontend/src/app/shared/models/enums/career-stage.enum.ts
@@ -5,4 +5,10 @@ export enum CareerStageEnum {
   EARLY_CAREER_RESEARCHERS = 'Early stage researchers',
   MID_CAREER_RESEARCHERS = 'Mid-career researchers',
   ESTABLISHED_CAREER_RESEARCHERS = 'Established researchers',
+  UNDERGRADUATE_STUDENTS = 'Undergraduate students',
+  GRADUATE_STUDENTS = 'Graduate students',
+  POSTGRADUATE_STUDENTS = 'Postgraduate students',
+  POSTDOCTORAL_RESEARCHERS = 'Postdoctoral researchers',
+  SCIENTISTS = 'Scientists',
+  SCIENTIFIC_INSTITUTION = 'Scientific institution',
 }
