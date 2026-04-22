a7f0d0a1514874b1c2bc4eee0103d3fed1ccdc0d
juliakummer <julia.kummer@andamp.io>
2026-04-22 15:27:37 +0200

    [CEFUN-147]: adjust required fields for funding and call stage schemas backend

--- backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/CallStageWebModel.java
+++ backend/adapters/src/main/java/at/ac/tuwien/fundify/adapters/in/rest/dto/CallStageWebModel.java
@@ -1,15 +1,12 @@
 package at.ac.tuwien.fundify.adapters.in.rest.dto;
 
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 
 public record CallStageWebModel(
         int number,
-        @Schema(required = true) @NotNull
         DateRangeWebModel duration,
-        @Schema(required = false)
         List<TranslatedTextWebModel> description
 ) {
 }
